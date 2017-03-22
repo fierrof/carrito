db2c9fbefb8b38dc4fa612bfe84571d58ceb82a7
fierrof <facufierro@gmail.com>
2017-03-22 19:18:38 -0300

    Add files via upload

--- Carrito/new_producto.php
+++ Carrito/new_producto.php
@@ -1,9 +1,44 @@
+<?php 
+include('dbConfig.php');
+session_start();
+
+if(isset($_POST['nombre']) && isset($_POST['cantidad']) && isset($_POST['precio']) && isset($_POST['image'])){
+
+	$nombre = $_POST['nombre'];
+	$cantidad = $_POST['cantidad'];
+	$precio = $_POST['precio'];		
+
+	$query = "INSERT INTO producto (nombre,cantidad,precio,image,id_usuario) VALUES ('$nombre','$cantidad','$precio','$image'," . $_SESSION['id_usuario'] . ")";
+
+	mysqli_query($connection,$query);
+	header("location:index.php");
+	
+}
+
+mysqli_close($connection);
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Nuevo Producto</title>
+	<title></title>
 </head>
 <body>
-
+	<h1>Signup</h1>
+	<form action="new_user.php" method="POST">
+		<p>Nombre del Producto:</p><input type="text" name="nombre">
+		<p>Cantidad en Stock:</p><input type="text" name="cantidad">
+		<p>Precio:</p><input type="text" name="precio">
+		<br>
+		<form action="upload.php" method="post" enctype="multipart/form-data">
+			Seleccionar imagen:
+			<br>
+			<input type="file" name="fileToUpload" id="fileToUpload">
+			<br>
+			<input type="submit" value="Upload Image" name="submit">
+		</form>
+		<br>
+		<input type="submit" value="Agregar">
+	</form>
 </body>
 </html>
