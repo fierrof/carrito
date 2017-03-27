eb11670cf812a5fae0af4577ad0cbad8b05b2c05
fierrof <facufierro@gmail.com>
2017-03-27 11:45:31 -0300

    subida de imagenes
    incompleto

--- Carrito/new_producto.php
+++ Carrito/new_producto.php
@@ -25,20 +25,18 @@ mysqli_close($connection);
 </head>
 <body>
 	<h1>Signup</h1>
+	<form action="upload.php" method="post" enctype="multipart/form-data">
+		<input type="file" name="fileToUpload" id="fileToUpload">
+		<input type="submit" value="Upload Image" name="submit">
+	</form>
 	<form action="new_user.php" method="POST">
 		<p>Nombre del Producto:</p><input type="text" name="nombre">
 		<p>Cantidad en Stock:</p><input type="text" name="cantidad">
 		<p>Precio:</p><input type="text" name="precio">
+		<p>Descripcion:</p><textarea type="text" name="descripcion"></textarea>
 		<br>
-		<form action="upload.php" method="post" enctype="multipart/form-data">
-			Seleccionar imagen:
-			<br>
-			<input type="file" name="fileToUpload" id="fileToUpload">
-			<br>
-			<input type="submit" value="Upload Image" name="submit">
-		</form>
-		<br>
+		<br>				
 		<input type="submit" value="Agregar">
 	</form>
-</body>
-</html>
+	</body>
+	</html>
