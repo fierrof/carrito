ddfce86874e910ee0097a2d9a90af38362749389
fierrof <facufierro@gmail.com>
2017-04-13 15:18:20 -0300

    Add files via upload

--- Carrito/index.php
+++ Carrito/index.php
@@ -6,11 +6,22 @@
 	
 </head>
 <body>
-	<form action="login.php" method="POST">
-		<input type="text" name="email" placeholder="E-Mail" />		
-		<input type="password" name="pass" placeholder="Password" />
-		<input type="submit" name="submit" value="Login" class="button1" />
-	</form>
-	<a href="new_user.php">Signup</a>
+	<div class="background" >
+		<div class="logo">
+			<img src="images/logo/logo.png" alt="logo">	
+		</div>
+		<div class="panel">			
+			<a href="catalogo.php" class="panel_btn">Home</a>			
+			<a href="contacto.php" class="panel_btn">Contacto</a>
+			<a href="logout.php" class="btn_logout"><img src="images/iconos/logout.png"></a>			
+		</div>
+		<form action="login.php" method="POST">
+			<br>
+			<input type="text" name="email" placeholder="E-Mail" /><br>
+			<input type="password" name="pass" placeholder="Password" /><br>
+			<input type="submit" name="submit" value="Login" class="button1" /><br>
+		</form>
+		<a href="new_user.php">Signup</a>
+	</div>
 </body>
 </html>
